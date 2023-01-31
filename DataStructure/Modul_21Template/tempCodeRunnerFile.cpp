b3d5fb0740e84ceba4c792ae54f6c67efd6e4b68
MSI75 <sisaif56@gmail.com>
2023-01-31 09:32:16 +0600

    prob_5

---
+++ DataStructure/Modul_21Template/tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+  // Stack<int> temp;
+
+    // while (st.getSize() > 0)
+    // {
+    //     int t = st.top();
+    //     st.pop();
+    //     while (temp.getSize() > 0)
+    //     {
+    //         if (temp.top() < t)
+    //         {
+    //             break;
+    //         }
+    //         st.push(temp.top());
+    //         temp.pop();
+    //     }
+    //     temp.push(t);
+    // }
+
+    // swap(st, temp);
