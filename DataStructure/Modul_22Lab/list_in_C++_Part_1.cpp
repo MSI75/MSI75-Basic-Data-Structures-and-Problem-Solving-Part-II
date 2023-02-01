f9b408ac02ee1ad3968194c1029adfa47873720a
MSI75 <sisaif56@gmail.com>
2023-02-01 14:39:09 +0600

    list in c++

--- DataStructure/Modul_22Lab/list_in_C++_Part_1.cpp
+++ DataStructure/Modul_22Lab/list_in_C++_Part_1.cpp
@@ -13,6 +13,19 @@ void print(list<int>l){
     cout<<"\n";
     
 }
+// O(n)
+void inseart(list<int>&l, int index, int value){
+    auto it = l.begin(); // O(1)
+    advance(it, index);  // O(n)
+    l.insert(it, value); // O(1)
+}
+
+// O(n)
+void Delete(list<int>&l, int index){
+    auto it = l.begin(); // O(1)
+    advance(it, index); // O(n)
+    l.erase(it); // O(1)
+}
 
 int main(){
     list<int>l;
@@ -32,4 +45,14 @@ int main(){
 
     l.pop_front();
     print(l);
+
+    inseart(l, 1, 100);
+    print(l);
+
+    Delete(l,2);
+    print(l);
+
+    cout<<l.size()<<"\n";
+
+
 }
