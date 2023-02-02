a9533d5a98935f41cb68e435d367022e22b05418
MSI75 <sisaif56@gmail.com>
2023-02-02 18:38:59 +0600

    Array of vector

---
+++ STL/ArrayOfVectors/ArrayOfVectors.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printVec(vector<int>&v){
+    cout<<"Size : "<<v.size()<<"\n";
+
+    for(int i = 0; i<v.size(); ++i){
+      cout<<v[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+int main(){
+    int N;
+    cin>>N;
+    vector<int>v[N];
+
+    for(int i = 0; i<N; ++i){
+        int n;
+        cin>>n;
+        for(int i = 0; i<n; ++i){
+            int x;
+            cin>>x;
+            v[i].push_back(x);
+        }
+    }
+
+    for(int i = 0; i<N; ++i){
+        printVec(v[i]);
+    }
+    return 0;
+}
