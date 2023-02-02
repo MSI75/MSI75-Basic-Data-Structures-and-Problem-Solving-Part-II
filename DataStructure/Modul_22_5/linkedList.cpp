4ec2d0ca9279151d9dcfc1a828bb39dca892fc10
MSI75 <sisaif56@gmail.com>
2023-02-02 08:14:42 +0600

    search and remove

--- DataStructure/Modul_22_5/linkedList.cpp
+++ DataStructure/Modul_22_5/linkedList.cpp
@@ -1,28 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void print(list<int>l){
+void print(list<int> &l)
+{
     auto it = l.begin();
-    while(it != l.end()){
-        cout<<*it<<" ";
+    while (it != l.end())
+    {
+        cout << *it << " ";
         it++;
     }
-    cout<<"\n";
+    cout << "\n";
 }
 
-int getValue(list<int>l,int index){
-   if(index>=l.size()) return -1;
+int getValue(list<int> &l, int index)
+{
+    if (index >= l.size())
+        return -1;
 
-   auto it = l.begin();
-   advance(it, index);
-   return *it;
-    
+    auto it = l.begin();
+    advance(it, index);
+    return *it;
 }
 
+bool search(list<int> &l, int value)
+{
 
-int main(){
-    list<int>l={3,2,6,4,5};
+    return find(l.begin(), l.end(), value) != l.end();
+}
+
+void deleteZero(list<int> &l)
+{
+    l.erase(remove(l.begin(), l.end(), 0), l.end());
+}
+int main()
+{
+    list<int> l = {3, 0, 2, 0, 6, 0, 4, 0, 5};
     print(l);
 
-    cout<<getValue(l, 3)<<"\n";
+    cout << getValue(l, 3) << "\n";
+
+    if (search(l, 5))
+    {
+        cout << "true\n";
+    }
+    else
+    {
+        cout << "false\n";
+    }
+
+    deleteZero(l);
+    print(l);
 }
