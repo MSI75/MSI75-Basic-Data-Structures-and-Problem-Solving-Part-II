437707902385b1240706ceecc20b48fd8bce7018
MSI75 <sisaif56@gmail.com>
2023-02-21 23:25:34 +0600

    set

---
+++ DataStructure/Modul_34STL/set.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    set<int> st;
+    // O(log(n))
+    st.insert(5);
+    st.insert(3);
+    st.insert(4);
+    st.insert(7);
+    st.insert(3);
+    st.insert(7);
+
+    // O(n)
+    // printing set
+    for (auto i : st)
+        cout << i << " ";
+    cout << "\n";
+
+    // O(1)
+    cout << "set size : " << st.size() << "\n";
+
+    // O(n)
+    // printing set
+    // for (auto it = st.begin(); it != st.end(); it++)
+    // {
+    //     cout << *it << " ";
+    // }
+    // cout << "\n";
+
+    // O(log(n))
+    // Erase
+    st.erase(5);
+    for (auto i : st)
+        cout << i << " ";
+    cout << "\n";
+
+    // st.erase(8);
+    // for (auto i : st)
+    //     cout << i << " ";
+    // cout << "\n";
+
+    // O(log(n))
+    if (st.find(7) != st.end())
+        cout << "7 is found\n";
+    else
+        cout << "7 is not found\n";
+
+    if (st.find(5) != st.end())
+        cout << "5 is found\n";
+    else
+        cout << "5 is not found\n";
+    return 0;
+}
---
+++ DataStructure/Modul_34STL/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+ // st.erase(8);
+    // for (auto i : st)
+    //     cout << i << " ";
+    // cout << "\n";
