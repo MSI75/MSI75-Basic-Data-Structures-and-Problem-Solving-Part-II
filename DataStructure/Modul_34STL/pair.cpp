515ee16214dfd5785204809a4e2fd908abdd7997
MSI75 <sisaif56@gmail.com>
2023-02-21 23:45:35 +0600

    pair

---
+++ DataStructure/Modul_34STL/pair.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    pair<string, int>p;
+
+    p.first = "abcd";
+    p.second = 3;
+
+    cout<<"Pair p = "<<p.first<<" , "<<p.second<<"\n";
+
+    pair<int, int>a;
+    a.first = 100;
+    a.second = 2;
+
+    cout<<"Pair a = "<<a.first<<" , "<<a.second<<"\n";
+}
