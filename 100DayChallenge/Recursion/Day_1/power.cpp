5a1f878629a828adf2edac60db183f2f6c1379f2
MSI75 <sisaif56@gmail.com>
2023-02-21 16:17:54 +0600

    power

---
+++ 100DayChallenge/Recursion/Day_1/power.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int power(int n){
+    // base case 
+    if(n==0) return 1;
+
+    // Recursive Relation 
+    // int smallProblem = power(n-1);
+    // int bigProblem = 2 * smallProblem;
+    // return bigProblem;
+
+    return 2*power(n-1);
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    int ans = power(n);
+    cout<<ans<<"\n";
+}
