fefd8202884c1968de2a401178a4c20635f3d7f4
MSI75 <sisaif56@gmail.com>
2023-02-22 16:40:39 +0600

    SayDigit

---
+++ 100DayChallenge/Recursion/Day_2/sayDigit.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sayDigit(int n, string arr[])
+{
+    if (n == 0)
+        return;
+
+    int digit = n % 10;
+    n = n / 10;
+    sayDigit(n, arr);
+    cout << arr[digit] << " ";
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    string arr[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+    sayDigit(n, arr);
+    return 0;
+}
