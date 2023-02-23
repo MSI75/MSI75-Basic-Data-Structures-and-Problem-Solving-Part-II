9d193855d5f6231c5a6854c7cec83c4a1680902e
MSI75 <sisaif56@gmail.com>
2023-02-24 03:25:33 +0600

    Bracket Sequence

---
+++ DataStructure/Modul_35_LabFinal/prob_4.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isBracketSequence(stack<int> st, string s)
+{
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        char ch = s[i];
+        if (ch == '(' || ch == '{' || ch == '[')
+        {
+            st.push(ch);
+        }
+        else
+        {
+            if (st.empty())
+                return false;
+
+            if (st.top() == '(' && ch == ')')
+                st.pop();
+            else if (st.top() == '{' && ch == '}')
+                st.pop();
+            else if (st.top() == '[' && ch == ']')
+                st.pop();
+            else
+                return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    stack<int> st;
+
+    if (isBracketSequence(st, s))
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+}
